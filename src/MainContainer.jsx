3a2afd21401ea8059d548eb6a9597294ea2c224f
Cristian Villa <c.villavirgen@gmail.com>
2024-02-04 04:57:25 -0600

    Add OrbitControls

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -1,10 +1,11 @@
 import { Canvas } from '@react-three/fiber';
-import { Stars } from '@react-three/drei';
+import { Stars, OrbitControls } from '@react-three/drei';
 
 const MainContainer = () => {
     return (
         <Canvas>
                 <color attach="background" args={['black']} />
+                <OrbitControls />
                 <Stars />
         </Canvas>
     );
