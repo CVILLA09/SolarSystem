78862f3f896aeb49046d8d32d31e47d4b53f94a4
Cristian Villa <c.villavirgen@gmail.com>
2024-02-04 04:56:20 -0600

    Add stars to the background in MainContainer

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -1,9 +1,11 @@
 import { Canvas } from '@react-three/fiber';
+import { Stars } from '@react-three/drei';
 
 const MainContainer = () => {
     return (
         <Canvas>
                 <color attach="background" args={['black']} />
+                <Stars />
         </Canvas>
     );
 }
