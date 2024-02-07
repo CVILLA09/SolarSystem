536c5d564f1d39392f845f2cf1f9c5e8e015261b
Cristian Villa <c.villavirgen@gmail.com>
2024-02-06 22:14:09 -0600

    Add rotation animation to earth

--- src/Earth.jsx
+++ src/Earth.jsx
@@ -1,14 +1,24 @@
 import { useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Earth = ({ displacementScale }) => {
-    const [earthTexture, earthNormalMap, earthSpecularMap, earthDisplacementMap] = useTexture([
+  const earthRef = useRef()
+
+    const [earthTexture, earthNormalMap, earthSpecularMap, earthDisplacementMap] = 
+    useTexture([
       '/assets/earth_day.jpg', 
       '/assets/earth_normal.jpg',
       '/assets/earth_specular.jpg',
       '/assets/earth_displacement.jpg'
     ]);
+
+    useFrame (() => {
+      earthRef.current.rotation.y += 0.002
+    })
+
   return (
-    <mesh>
+    <mesh ref={earthRef}>
         {/* Radius, X-axis, Y-axis  */}
         <sphereGeometry args={[1, 32, 32]} />
           <meshPhongMaterial 
