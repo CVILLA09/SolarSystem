f221e86b204f72f6bc3793b7f396c70bed65a423
Cristian Villa <c.villavirgen@gmail.com>
2024-02-07 02:46:18 -0600

    Final details of Axis variables

--- src/ISS.jsx
+++ src/ISS.jsx
@@ -1,13 +1,27 @@
 import { useGLTF } from '@react-three/drei';
-import { useMemo } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useMemo, useRef } from 'react';
 
 const ISS = () => {
+    const ISSRef = useRef();
     const memoizedISS = useMemo(() => {
         return useGLTF('/ISSModel/ISS_stationary.gltf');
     });
+
+    const xAxis = 2
+    useFrame (({clock}) => {
+        // Orbit Rotation
+        ISSRef.current.position.x = Math.sin(clock.getElapsedTime() * 0.4) * xAxis
+        ISSRef.current.position.z = Math.cos(clock.getElapsedTime() * 0.4) * xAxis
+      })
+
     return(
         <mesh>
-            <primitive object={memoizedISS.scene} position={[2,0,0]} scale={0.003} />
+            <primitive 
+            ref={ISSRef}
+            object={memoizedISS.scene} 
+            position={[xAxis,0,0]} 
+            scale={0.003} />
         </mesh>
     )
 };
--- src/Moon.jsx
+++ src/Moon.jsx
@@ -7,16 +7,17 @@ const Moon = () => {
 
     const [moonTexture] = useTexture(['/assets/moon.jpg']);
 
+    const xAxis = 4
     useFrame (({clock}) => {
       // Orbit Rotation
-      moonRef.current.position.x = Math.sin(clock.getElapsedTime() * 0.2) * 4
-      moonRef.current.position.z = Math.cos(clock.getElapsedTime() * 0.2) * 4
+      moonRef.current.position.x = Math.sin(clock.getElapsedTime() * 0.2) * xAxis
+      moonRef.current.position.z = Math.cos(clock.getElapsedTime() * 0.2) * xAxis
       // Axis Rotation 
       moonRef.current.rotation.y += 0.001
     })
 
   return (
-    <mesh castShadow ref={moonRef} position={[3.8,0,0]}>
+    <mesh castShadow ref={moonRef} position={[xAxis,0,0]}>
         {/* Radius, X-axis, Y-axis  */}
         <sphereGeometry args={[0.27, 32, 32]} />
           <meshPhongMaterial 
