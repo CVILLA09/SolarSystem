37f61b65ec04c13f88e6b59f493ebd594747e124
Cristian Villa <c.villavirgen@gmail.com>
2024-02-06 22:39:47 -0600

    Install r3f-perf - performance tracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
 import MainContainer from "./MainContainer";
 
 function App() {
 
   return (
     <Canvas camera={{fov: 75, near: 0.1, far: 1000, position: [0,3,3] }}>
+      <Perf />
       <OrbitControls />
       <MainContainer />
     </Canvas>
