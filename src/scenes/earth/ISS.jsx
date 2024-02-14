9336e082652b3ac2bc5712788562e869a3a1e114
Cristian Villa <c.villavirgen@gmail.com>
2024-02-13 19:35:21 -0600

    Update ISS component useMemo with React.memo

--- src/scenes/earth/ISS.jsx
+++ src/scenes/earth/ISS.jsx
@@ -1,19 +1,20 @@
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
-import { useMemo, useRef } from 'react';
+import React, { useMemo, useRef, useCallback } from 'react';
 
-const ISS = () => {
+const ISS = React.memo(() => {
     const ISSRef = useRef();
     const memoizedISS = useMemo(() => {
         return useGLTF('/ISSModel/ISS_stationary.gltf');
     });
 
-    const xAxis = 2
-    useFrame (({clock}) => {
+    const updateMoonPosition = useCallback(() => {
+        const xAxis = 2
         // Orbit Rotation
         ISSRef.current.position.x = Math.sin(clock.getElapsedTime() * 1.2) * xAxis
         ISSRef.current.position.z = Math.cos(clock.getElapsedTime() * 1.2) * xAxis
-      })
+    }, [])
+    useFrame (() => {updateMoonPosition()})
 
     return(
         <mesh>
@@ -24,6 +25,6 @@ const ISS = () => {
             scale={0.003} />
         </mesh>
     )
-};
+});
 
 export default ISS;
