abd3ea4c81831f6d4a134020242b774c61c165d4
Cristian Villa <c.villavirgen@gmail.com>
2024-02-22 22:14:04 -0600

    clear a little

--- src/MainContainer.jsx
+++ src/MainContainer.jsx
@@ -3,33 +3,17 @@ import AnimatedStars from './AnimatedStars';
 import { useRef } from 'react';
 import { Perf } from 'r3f-perf';
 
-import * as THREE from 'three';
-
 import CameraPositionLogging from './helpers/CameraPositionLogging';
 
 import Earth from './scenes/earth/Earth';
 import Sun from './scenes/sun/Sun';
 
 const MainContainer = () => {
-    const directionalLightRef = useRef();
-    const directionalLightRefTwo = useRef();
-    useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1, 'hotpink');
-    useHelper(directionalLightRefTwo, THREE.DirectionalLightHelper, 1, 'hotpink');
     return (
             <>
                 <Perf />
                 <CameraPositionLogging event="mousedown" />
                 <AnimatedStars />
-                {/* <directionalLight 
-                castShadow
-                ref={directionalLightRef} 
-                position={[0,0,10]} 
-                intensity={3}/>
-                <directionalLight 
-                castShadow
-                ref={directionalLightRefTwo} 
-                position={[0,0,-10]} 
-                intensity={3}/> */}
                 <ambientLight intensity={0.005} />
                 <Sun />
                 <Earth displacementScale={0.03}/>
